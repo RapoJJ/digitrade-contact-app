c330c7ce99a664ce1fbb037a78b6b5a7707f5647
udwee <jonttu.92@hotmail.com>
2019-05-17 11:54:44 +0300

    app renamed to dtca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'dtca-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'Kontakti sovellus 2019';
 }
