0508db1d82f939f194b5bec8546cddf140a172b8
udwee <jonttu.92@hotmail.com>
2019-05-17 12:39:54 +0300

    contact-list-item print works

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to contact-webapp!');
+    expect(page.getTitleText()).toEqual('Welcome to c-webapp!');
   });
 
   afterEach(async () => {
