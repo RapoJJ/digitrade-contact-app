335f419a6d67ac3986edbb30650d25ca63054d05
udwee <jonttu.92@hotmail.com>
2019-05-21 12:08:41 +0300

    flex-layout installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import {FormsModule} from '@angular/forms';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
 import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
 import {ContactDetailComponent} from './contact/contact-detail/contact-detail.component';
-import {MatButtonModule, MatButtonToggleModule, MatCardModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @NgModule({
@@ -23,7 +24,7 @@ import {MatButtonModule, MatButtonToggleModule, MatCardModule, MatFormFieldModul
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatButtonToggleModule
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
