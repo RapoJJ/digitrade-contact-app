57fcaf87907c6d2415458fb63ee77ad08f3b6885
udwee <jonttu.92@hotmail.com>
2019-05-21 10:46:23 +0300

    Testing premade html modules from angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,25 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
 import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material';
+import {ContactDetailComponent} from './contact/contact-detail/contact-detail.component';
+import {MatButtonModule, MatButtonToggleModule, MatCardModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
-    ContactListItemComponent
+    ContactListItemComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     MatCardModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -2,10 +2,20 @@ export class Contact {
   firstName: string;
   lastName: string;
   phone: string;
+  email: string;
+  address: IAddress;
 
-  constructor(firstName?: string, lastName?: string, phone?: string) {
+  constructor(firstName?: string, lastName?: string, phone?: string, email?: string, address?: IAddress) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
+    this.email = email;
+    this.address = address;
   }
+}
+
+interface IAddress {
+  street: string;
+  postalCode: string;
+  city: string;
 }
